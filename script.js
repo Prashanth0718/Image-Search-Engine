4276d4291d3d1cdda776809568cca0a65b7fea40
Prashanth S N <163889724+Prashanth0718@users.noreply.github.com>
2024-08-16 18:09:20 +0530

    getting photos from api

---
+++ script.js
@@ -0,0 +1,13 @@
+const apiKey = "R99gFkpqFYrrct89nximMKplrJ0ElrmlVtPBrveqTiJx8SkGLs5GsoMp";
+const perPage = 15 //we will load 15 images on every API call
+let currentPage = 1;
+
+const getImages = (apiURL) => {
+    fetch(apiURL, {
+        headers: { Authorization: apiKey }
+    }).then(res => res.json()).then(data => {
+        generateHTML(data.photos);
+    })
+}
+
+getImages(`https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`);
