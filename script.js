b128df53bc8ccf71b2cdb589452110ab12e6802e
Prashanth S N <163889724+Prashanth0718@users.noreply.github.com>
2024-08-16 19:00:11 +0530

    loading images to webpage

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
+const imagesWrapper = document.querySelector(".images");
+
 const apiKey = "R99gFkpqFYrrct89nximMKplrJ0ElrmlVtPBrveqTiJx8SkGLs5GsoMp";
 const perPage = 15 //we will load 15 images on every API call
 let currentPage = 1;
+
+const generateHTML = (images) => {
+    //Making li of all fetched images and adding them to the exiting image wrapper
+    imagesWrapper.innerHTML += images.map(img => 
+        ` <li class="card">
+                <img src="${img.src.large2x}" alt="img">
+                <div class="details">
+                    <div class="photographer">
+                        <i class="uil uil-camera"></i>
+                        <span>${img.photographer}</span>
+                    </div>
+                    <button><i class="uil uil-import"></i></button>
+                </div>
+            </li>`
+    ).join("");
+}
 
 const getImages = (apiURL) => {
     fetch(apiURL, {
         headers: { Authorization: apiKey }
     }).then(res => res.json()).then(data => {
-        console.log(data);
+        generateHTML(data.photos);
     })
 }
 
